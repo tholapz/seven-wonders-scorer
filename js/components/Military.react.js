a500025f4cda80b16f8caa147369b5a652ce50e0
Kamol Treewatchararat <kamol.t@vixlet.com>
2016-01-15 16:13:18 -0800

    add input fields for military

--- js/components/Military.react.js
+++ js/components/Military.react.js
@@ -1,29 +1,31 @@
 var React = require('react');
-var $ = require('jquery');
-$.slider = require('bootstrap-slider');
+var assign = require('object-assign');
 
 var Military = React.createClass({
-	componentDidMount: function() {
-		debugger;
-		$('#ex1').slider({
-			formatter: function(value) {
-				return 'Current value: ' + value;
-			}
-		});
+
+	getInitialState: function() {
+		return {
+			minusOne: 0,
+			plusOne: 0,
+			plusThree: 0,
+			plusFive: 0
+		}
 	},
 
+	handleChangeFactory: function(stateName) {
+		return function(e) {
+			var newState = assign({}, this.state);
+			newState[stateName] = e.currentTarget.value;
+			this.setState(newState);
+		}.bind(this);
+	},
 
 	render: function() {
 		return (<form>
-			<input
-				id="ex1"
-				data-slider-id="ex1Slider"
-				type="text"
-				data-slider-min="0"
-				data-slider-max="20"
-				data-slider-step="1"
-				data-slider-value="14"
-			/>
+			<label>-1</label><input value={this.state.minusOne} onChange={this.handleChangeFactory.call(this,'minusOne')} type="number"/><br/>
+			<label>1</label><input value={this.state.plusOne} onChange={this.handleChangeFactory.call(this,'plusOne')} type="number"/><br/>
+			<label>3</label><input value={this.state.plusThree} onChange={this.handleChangeFactory.call(this,'plusThree')} type="number"/><br/>
+			<label>5</label><input value={this.state.plusFive} onChange={this.handleChangeFactory.call(this,'plusFive')} type="number"/><br/>
 		</form>);
 	}
 });
